04f947fb019092a1c7b7bd43bc114693003b3a03
Craigmillion <choycraig@yahoo.com>
2026-06-21 19:14:35 +0800

    wrap useParams pages in Suspense block

--- src/app/(public)/login/page.tsx
+++ src/app/(public)/login/page.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next';
+import { Suspense } from 'react';
 import AuthScreen from '../../../components/auth/AuthScreen';
 
 export const metadata: Metadata = {
@@ -7,5 +8,9 @@ export const metadata: Metadata = {
 };
 
 export default function LoginPage() {
-  return <AuthScreen mode="login" layout="centered" switchHref="/signup" />;
+  return (
+    <Suspense>
+      <AuthScreen mode="login" layout="centered" switchHref="/signup" />
+    </Suspense>
+  );
 }
